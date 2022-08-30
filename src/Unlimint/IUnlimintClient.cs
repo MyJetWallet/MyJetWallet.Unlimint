7b7c53c08c2829cea63adbd1e2cb39e3c2bbe934
yuriy-smplt <yuriy.p@smplt.net>
2022-08-30 17:04:17 +0300

    apple pay added

--- src/Unlimint/IUnlimintClient.cs
+++ src/Unlimint/IUnlimintClient.cs
@@ -155,7 +155,6 @@ namespace MyJetWallet.Unlimint
         /// <summary>
         /// Create a payment.
         /// </summary>
-        /// <param name="paymentAlternativeType"></param>
         /// <param name="merchantOrderId">Unique idempotency key. This key is utilized to ensure exactly-once execution of mutating requests.</param>
         /// <param name="requestId">Unique identifier of the public key used in encryption.</param>
         /// <param name="amount">Magnitude of the amount, in units of the currency, with a ..</param>
@@ -168,15 +167,8 @@ namespace MyJetWallet.Unlimint
         /// <param name="verificationUrlInProcess"></param>
         /// <param name="verificationUrlReturn"></param>
         /// <param name="time"></param>
-        /// <param name="livingAddress"></param>
         /// <param name="customer"></param>
         /// <param name="cancellationToken"></param>
-        /// <param name="email">Email of the user</param>
-        /// <param name="phoneNumber">Phone number of the user in E.164 format. We recommend using a library such as libphonenumber to parse and validate phone numbers.</param>
-        /// <param name="sourceId">Unique identifier for the source.</param>
-        /// <param name="cardToken"></param>
-        /// <param name="threeDsChallengeIndicator"></param>
-        /// <param name="paymentMethod"></param>
         /// <returns></returns>
         WebCallResult<PaymentGatewayCreationResponse> CreateAlternativePayment(
             List<string> alternativeMethods,
@@ -218,15 +210,9 @@ namespace MyJetWallet.Unlimint
         /// </summary>
         /// <param name="merchantOrderId">Unique idempotency key. This key is utilized to ensure exactly-once execution of mutating requests.</param>
         /// <param name="requestId">Unique identifier of the public key used in encryption.</param>
-        /// <param name="email">Email of the user</param>
-        /// <param name="phoneNumber">Phone number of the user in E.164 format. We recommend using a library such as libphonenumber to parse and validate phone numbers.</param>
-        /// <param name="sessionId">Hash of the session identifier; typically of the end user. This helps us make risk decisions and prevent fraud. IMPORTANT: Please hash the session identifier to prevent sending us actual session identifiers.</param>
-        /// <param name="ipAddress">Single IPv4 or IPv6 address of user</param>
         /// <param name="amount">Magnitude of the amount, in units of the currency, with a ..</param>
         /// <param name="currency">Currency code.</param>
-        /// <param name="sourceId">Unique identifier for the source.</param>
         /// <param name="generateToken"></param>
-        /// <param name="threeDsChallengeIndicator"></param>
         /// <param name="useThreeDsChallengeIndicator"></param>
         /// <param name="description">Description of the payment with length restriction of 240 characters.</param>
         /// <param name="verificationUrlSuccess"></param>
@@ -235,8 +221,6 @@ namespace MyJetWallet.Unlimint
         /// <param name="verificationUrlInProcess"></param>
         /// <param name="verificationUrlReturn"></param>
         /// <param name="time"></param>
-        /// <param name="paymentMethod"></param>
-        /// <param name="clientId"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         WebCallResult<PaymentGatewayCreationResponse> CreateGatewayBankCardPayment(string merchantOrderId,
@@ -274,6 +258,40 @@ namespace MyJetWallet.Unlimint
             PaymentRequestCardAccount cardAccount,
             CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Create a payment.
+        /// </summary>
+        /// <param name="merchantOrderId">Unique idempotency key. This key is utilized to ensure exactly-once execution of mutating requests.</param>
+        /// <param name="requestId">Unique identifier of the public key used in encryption.</param>
+        /// <param name="amount">Magnitude of the amount, in units of the currency, with a ..</param>
+        /// <param name="currency">Currency code.</param>
+        /// <param name="description">Description of the payment with length restriction of 240 characters.</param>
+        /// <param name="appleEncryptedData"></param>
+        /// <param name="verificationUrlSuccess"></param>
+        /// <param name="verificationUrlFailure"></param>
+        /// <param name="verificationUrlCancel"></param>
+        /// <param name="verificationUrlInProcess"></param>
+        /// <param name="verificationUrlReturn"></param>
+        /// <param name="time"></param>
+        /// <param name="customer"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        WebCallResult<PaymentGatewayCreationResponse> CreateGatewayApplepayPayment(
+             string merchantOrderId,
+             string requestId, decimal amount, string currency, string description, 
+             string appleEncryptedData, string verificationUrlSuccess,
+             string verificationUrlFailure, string verificationUrlCancel, string verificationUrlInProcess,
+             string verificationUrlReturn, DateTime time, PaymentRequestCustomer customer,
+             CancellationToken cancellationToken = default);
+
+        Task<WebCallResult<PaymentGatewayCreationResponse>> CreateGatewayApplepayPaymentAsync(
+            string merchantOrderId,
+            string requestId, decimal amount, string currency, string description, 
+            string appleEncryptedData, string verificationUrlSuccess,
+            string verificationUrlFailure, string verificationUrlCancel, string verificationUrlInProcess,
+            string verificationUrlReturn, DateTime time, PaymentRequestCustomer customer,
+            CancellationToken cancellationToken = default);
+        
         /// <summary>
         /// Get a payment.
         /// </summary>
