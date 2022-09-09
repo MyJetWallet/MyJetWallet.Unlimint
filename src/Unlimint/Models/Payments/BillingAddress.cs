201b4afed441c57c927b3d20de00d1c57d778ff5
yuriy-smplt <yuriy.p@smplt.net>
2022-09-09 12:25:12 +0300

    updated json address fields

--- src/Unlimint/Models/Payments/BillingAddress.cs
+++ src/Unlimint/Models/Payments/BillingAddress.cs
@@ -4,10 +4,16 @@ namespace MyJetWallet.Unlimint.Models.Payments;
 
 public class BillingAddress
 {
-    [JsonProperty("addr_line_1")] public string AddrLine1 { get; set; }
-    [JsonProperty("addr_line_2")] public string AddrLine2 { get; set; }
-    [JsonProperty("city")] public string City { get; set; }
-    [JsonProperty("country")] public string Country { get; set; }
-    [JsonProperty("state")] public string State { get; set; }
-    [JsonProperty("zip")] public string Zip { get; set; }
+    [JsonProperty("addr_line_1", NullValueHandling = NullValueHandling.Ignore)]
+    public string AddrLine1 { get; set; }
+    [JsonProperty("addr_line_2", NullValueHandling = NullValueHandling.Ignore)] 
+    public string AddrLine2 { get; set; }
+    [JsonProperty("city", NullValueHandling = NullValueHandling.Ignore)] 
+    public string City { get; set; }
+    [JsonProperty("country", NullValueHandling = NullValueHandling.Ignore)] 
+    public string Country { get; set; }
+    [JsonProperty("state", NullValueHandling = NullValueHandling.Ignore)] 
+    public string State { get; set; }
+    [JsonProperty("zip", NullValueHandling = NullValueHandling.Ignore)] 
+    public string Zip { get; set; }
 }
--- src/Unlimint/Models/Payments/PaymentRequestLivingAddress.cs
+++ src/Unlimint/Models/Payments/PaymentRequestLivingAddress.cs
@@ -4,14 +4,14 @@ namespace MyJetWallet.Unlimint.Models.Payments;
 
 public class PaymentRequestLivingAddress
 {
-    [JsonProperty("address")]
+    [JsonProperty("address", NullValueHandling = NullValueHandling.Ignore)]
     public string Address { get; set; }
-    [JsonProperty("city")]
+    [JsonProperty("city", NullValueHandling = NullValueHandling.Ignore)]
     public string City { get; set; }
-    [JsonProperty("country")]
+    [JsonProperty("country", NullValueHandling = NullValueHandling.Ignore)]
     public string Country { get; set; }
-    [JsonProperty("state")]
+    [JsonProperty("state", NullValueHandling = NullValueHandling.Ignore)]
     public string State { get; set; }
-    [JsonProperty("zip")]
+    [JsonProperty("zip", NullValueHandling = NullValueHandling.Ignore)]
     public string Zip { get; set; }
 }
