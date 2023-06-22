b6ebebc524863ad024deb27f016c3edc95646348
yuriy-smplt <yuriy.p@smplt.net>
2023-06-22 18:02:38 +0300

    updated ConvenienceStore

--- src/MyJetWallet.Unlimint/MyNoSql/ConvenienceStoreNoSqlEntity.cs
+++ src/MyJetWallet.Unlimint/MyNoSql/ConvenienceStoreNoSqlEntity.cs
@@ -6,24 +6,23 @@ namespace MyJetWallet.Unlimint.MyNoSql
     {
         public const string TableName = "myjetwallet-unlimint-convenience-store-methods";
         public static string GeneratePartitionKey(string country) => country;
-        public static string GenerateRowKey(string country, string unlimintMethodId) => $"UCS-{unlimintMethodId.ToUpper()}-{country.ToUpper()}";
+        public static string GenerateRowKey(string unlimintMethodName) => unlimintMethodName;
 
-        public static ConvenienceStoreNoSqlEntity Create(string unlimintMethodId, string country, string currency, bool enabled)
+        public static ConvenienceStoreNoSqlEntity Create(string unlimintMethodName, int id, string country, string currency, bool enabled)
         {
-            var rowKey = GenerateRowKey(country, unlimintMethodId);
             return new ConvenienceStoreNoSqlEntity()
             {
                 PartitionKey = GeneratePartitionKey(country),
-                RowKey = rowKey,
-                Id = rowKey,
-                UnlimintMethodId = unlimintMethodId,
+                RowKey = GenerateRowKey(unlimintMethodName),
+                Id = id,
+                UnlimintMethodId = unlimintMethodName,
                 Country = country,
                 Currency = currency,
                 Enabled = enabled
             };
         }
 
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string UnlimintMethodId { get; set;}
         public string Country { get; set;}
         public string Currency { get; set;}
